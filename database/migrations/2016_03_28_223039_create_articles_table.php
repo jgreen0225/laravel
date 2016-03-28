01c8ec88709e32412eb9005edd026f4ca645ba96
jgreen0225 <jgreen0225@hotmail.com>
2016-03-28 18:35:53 -0400

    controller update

--- database/migrations/2016_03_28_223039_create_articles_table.php
+++ database/migrations/2016_03_28_223039_create_articles_table.php
@@ -14,7 +14,10 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->string('body');
             $table->timestamps();
+            $table->timestamps('published_at');
         });
     }
 

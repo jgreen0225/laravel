7f0f522e71d5c15d3365f1a22c0af996c84c8280
jgreen0225 <jgreen0225@hotmail.com>
2016-03-28 15:32:05 -0400

    controller update

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -5,6 +5,7 @@ class PagesController extends Controller {
 
     public function about()
         {
-            return view('pages.about');
+            $name = 'Jeffery Way';
+            return view('pages.about')->with('name', $name);
         }
 }

475aaec49213491e57317bf5fe4e4de87cb23f4d
jgreen0225 <jgreen0225@hotmail.com>
2016-03-28 16:00:56 -0400

    controller update

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,10 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>About Me</title>
-</head>
-<body>
+@extends('app')
+
+@section('content')
 <h1>About Me: {{$first}} {{$last}}</h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod ipsum arcu, et maximus erat tincidunt non. Quisque at nibh vel elit condimentum tincidunt. Vivamus id mauris condimentum, molestie massa nec, auctor nunc. Mauris vitae vehicula magna. Vestibulum vitae accumsan leo, sit amet euismod quam. Sed sagittis lectus urna, vitae vulputate velit consectetur vel. Fusce eleifend a nisi eu placerat.
 
@@ -15,6 +11,4 @@
     Praesent nulla diam, pulvinar ut luctus sed, scelerisque vel orci. Quisque nec sapien nunc. Curabitur porttitor rhoncus felis non placerat. Morbi dictum velit ac facilisis rhoncus. Sed metus lacus, auctor eu feugiat et, consequat a dolor. Vivamus dapibus, purus scelerisque dapibus pretium, felis libero eleifend risus, nec mollis ex mauris vel lectus. Maecenas lorem nisi, eleifend ac vehicula sed, tempor vitae elit. Praesent placerat accumsan dui, in commodo diam tempor finibus. Proin quis finibus arcu, in fringilla purus. Suspendisse venenatis aliquam mi, vitae gravida nisi imperdiet vitae. Proin magna erat, bibendum eu laoreet quis, consequat pharetra elit. Integer vitae ex lobortis, varius nisi vestibulum, tristique tortor. Nulla nec risus ac metus malesuada euismod. Duis ultrices ullamcorper mi, eget vulputate lectus interdum non.
 
     Nam imperdiet risus et risus auctor, et tempus orci porttitor. Sed consequat ultrices interdum. Sed mollis sem nec ex aliquam fermentum. Donec elit magna, rhoncus aliquet dignissim a, fringilla vel nisi. Mauris ultricies nibh id condimentum interdum. In dignissim gravida felis, id fermentum lorem consequat placerat. Donec lobortis justo vitae mi congue, nec pharetra risus condimentum. Morbi tempor quam in urna interdum, quis vestibulum enim faucibus. Nunc vitae justo nec nisl imperdiet bibendum. Ut ut fringilla arcu. Integer id leo rhoncus leo rhoncus convallis. Suspendisse condimentum scelerisque nisi vel ultricies. Quisque non mauris porttitor, blandit ante in, venenatis risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam maximus lectus in enim sollicitudin varius.</p>
-
-</body>
-</html>
+@stop

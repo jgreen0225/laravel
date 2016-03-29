b18d572cba3ec4f07c4e94d73177112c8e801d8f
jgreen0225 <jgreen0225@hotmail.com>
2016-03-29 18:43:13 -0400

    added tasks table

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,4 @@ Route::get('/', 'PagesController@index');
 Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
 Route::get('articles', 'ArticlesController@index');
+Route::get('articles/{id}', 'ArticlesController@show');
